7b8849667dc811e87add267cd509f618a36429c9
LuizFerK <luizfernandoklein@live.com>
2021-05-11 22:37:16 -0300

    test: Auth

--- test/elixirgithub_web/controllers/users_controller_test.exs
+++ test/elixirgithub_web/controllers/users_controller_test.exs
@@ -26,28 +26,85 @@ defmodule ElixirgithubWeb.UsersControllerTest do
   end
 
   describe "delete/2" do
-    setup %{conn: conn} do
+    setup do
       {:ok, %User{id: user_id} = user} = Create.call(%{"password" => "123456"})
 
-      {:ok, token, _claims} = Guardian.encode_and_sign(user)
-      conn = put_req_header(conn, "authorization", "Bearer #{token}")
-
-      {:ok, conn: conn, user_id: user_id}
+      {:ok, user: user, user_id: user_id}
     end
 
-    test "when the given user exists, delete the user", %{conn: conn, user_id: user_id} do
+    test "when the given user exists, delete the user", %{
+      conn: conn,
+      user: user,
+      user_id: user_id
+    } do
+      {:ok, token, _claims} = Guardian.encode_and_sign(user)
+
       response =
         conn
+        |> put_req_header("authorization", "Bearer #{token}")
         |> delete(Routes.users_path(conn, :delete, user_id))
         |> response(:no_content)
 
       assert response == ""
     end
 
-    test "when the given user does not exists, returns an error", %{conn: conn} do
+    test "when the given user does not exists, returns an error", %{conn: conn, user: user} do
+      {:ok, token, _claims} = Guardian.encode_and_sign(user)
+
+      response =
+        conn
+        |> put_req_header("authorization", "Bearer #{token}")
+        |> delete(Routes.users_path(conn, :delete, "7caa0719-c55e-45ca-9273-11fd664738c4"))
+        |> json_response(:not_found)
+
+      assert %{"error" => "User not found"} = response
+    end
+
+    test "when the user was not authenticated, returns an error", %{conn: conn} do
       response =
         conn
         |> delete(Routes.users_path(conn, :delete, "7caa0719-c55e-45ca-9273-11fd664738c4"))
+        |> json_response(:unauthorized)
+
+      assert %{"message" => "unauthenticated"} = response
+    end
+  end
+
+  describe "sign_in/2" do
+    setup do
+      {:ok, %User{id: user_id}} = Create.call(%{"password" => "123456"})
+
+      {:ok, id: user_id}
+    end
+
+    test "when the credentials are valid, authenticate the user", %{conn: conn, id: id} do
+      params = %{"id" => id, "password" => "123456"}
+
+      response =
+        conn
+        |> post(Routes.users_path(conn, :sign_in, params))
+        |> json_response(:ok)
+
+      assert %{"token" => _} = response
+    end
+
+    test "when the credentials are invalid, returns an error", %{conn: conn, id: id} do
+      params = %{"id" => id, "password" => "wrong_password"}
+
+      response =
+        conn
+        |> post(Routes.users_path(conn, :sign_in, params))
+        |> json_response(:unauthorized)
+
+      assert %{"error" => "Please verify your credentials"} = response
+    end
+
+    test "when the user does not exists, returns an error", %{conn: conn} do
+      params = %{"id" => "2baadea4-1d22-4d8c-9455-2ea5d692f930", "password" => "wrong_password"}
+
+      response =
+        conn
+        |> post(Routes.users_path(conn, :sign_in, params))
         |> json_response(:not_found)
 
       assert %{"error" => "User not found"} = response
